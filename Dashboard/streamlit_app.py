275237aedd6e4f0c3511d506718608fd53078fbb
mrarnoldy18 <145279141+mrarnoldy18@users.noreply.github.com>
2026-03-10 22:52:55 +0700

    Refactor streamlit_app.py for improved clarity

--- Dashboard/streamlit_app.py
+++ Dashboard/streamlit_app.py
@@ -13,7 +13,7 @@ st.title("📊 DASHBOARD SOIL MOISTURE")
 # ===============================
 # LOAD DATA
 # ===============================
-df = pd.read_csv("Outputs\data_soil_moisture_clean.csv")
+df = pd.read_csv("data_soil_moisture_clean.csv")
 
 # ===============================
 # HITUNG RATA-RATA
@@ -96,4 +96,5 @@ st.markdown(f"""
 • Tidak terdapat indikasi **tanah terlalu kering atau terlalu basah**
 
 """)
+
 

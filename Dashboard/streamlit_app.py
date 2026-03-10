ec8b1f5e5725a490b24e34baf18bbc83912d73b7
mrarnoldy18 <145279141+mrarnoldy18@users.noreply.github.com>
2026-03-10 22:55:53 +0700

    Update streamlit_app.py

--- Dashboard/streamlit_app.py
+++ Dashboard/streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from pathlib import Path
 
 # ===============================
 # CONFIG PAGE
@@ -102,6 +103,7 @@ st.markdown(f"""
 • Tidak terdapat indikasi **tanah terlalu kering atau terlalu basah**
 
 """)
+
 
 
 
